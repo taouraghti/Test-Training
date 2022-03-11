c106f9729e776260c32899c0ad7e97130a884f8a
anas idrissi <anasidrissi951@gmail.com>
2022-03-11 12:42:21 +0100

    step-8

--- src/main/java/FizzBuzzConverter.java
+++ src/main/java/FizzBuzzConverter.java
@@ -1,11 +1,11 @@
 public class FizzBuzzConverter {
     public String convert(int i) {
-        if(i % 5 == 0 && i % 3 != 0)
-            return "Buzz";
-        else if(i % 3 == 0 && i % 5 != 0)
-            return "Fizz";
-        else if(i % 3 == 0 && i % 5 == 0)
+        if(i % 3 == 0 && i % 5 == 0)
             return "FizzBuzz";
+        else if(i % 3 == 0)
+            return "Fizz";
+        else if(i % 5 == 0)
+            return "Buzz";
         else
             return String.valueOf(i);
     }
