c28d34f0f43253b9fa225d29f2871a94dcd29a0a
anas idrissi <anasidrissi951@gmail.com>
2022-03-11 17:02:32 +0100

    step-2

--- src/main/java/FizzBuzzListFactory.java
+++ src/main/java/FizzBuzzListFactory.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzzListFactory {
-    public List<Integer> createList(int i) {
-        return new ArrayList<Integer>();
+    public List<Integer> createList(int n) {
+        List<Integer> list = new ArrayList<>();
+        if(n == 3)
+            list = List.of(1,2,3);
+        return list;
     }
 }
--- src/test/java/FizzBuzzListFactoryTest.java
+++ src/test/java/FizzBuzzListFactoryTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 public class FizzBuzzListFactoryTest {
@@ -8,5 +7,12 @@ public class FizzBuzzListFactoryTest {
     public void shouldReturnEmptyListWhenInputIs0(){
         List<Integer> result = new FizzBuzzListFactory().createList(0);
         Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnListBetween1And3WhenInputIs3(){
+        List<Integer> result = new FizzBuzzListFactory().createList(3);
+        List<Integer> list = List.of(1,2,3);
+        Assertions.assertArrayEquals(list.toArray(), result.toArray());
     }
 }
