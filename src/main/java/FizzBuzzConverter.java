717d4223a969933d473c0423288aa750f0af9615
anas idrissi <anasidrissi951@gmail.com>
2022-03-11 12:23:14 +0100

    step-3

--- src/main/java/FizzBuzzConverter.java
+++ src/main/java/FizzBuzzConverter.java
@@ -2,6 +2,8 @@ public class FizzBuzzConverter {
     public String convert(int i) {
         if(i == 5)
             return "Buzz";
+        if(i == 15)
+            return "FizzBuzz";
         return "Fizz";
     }
 }
--- src/test/java/FizzBuzzConverterTest.java
+++ src/test/java/FizzBuzzConverterTest.java
@@ -8,10 +8,16 @@ public class FizzBuzzConverterTest {
         String result = new FizzBuzzConverter().convert(3);
         Assertions.assertEquals("Fizz", result);
     }
-    
+
     @Test
     public void shouldReturnBuzzWhenInputIs5(){
         String result = new FizzBuzzConverter().convert(5);
         Assertions.assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzWhenInputIs15(){
+        String result = new FizzBuzzConverter().convert(15);
+        Assertions.assertEquals("FizzBuzz", result);
     }
 }
